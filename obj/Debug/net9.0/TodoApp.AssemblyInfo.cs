4e17e78da80a5d1c9b941b725dd14222c6dfd856
konstant1128 <sk292824489@gmail.com>
2026-06-28 22:06:37 +0300

    Inizialize repository

--- obj/Debug/net9.0/TodoApp.AssemblyInfo.cs
+++ obj/Debug/net9.0/TodoApp.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TodoApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+03776d58021b4f442520e82ac15ce3a572c7c97c")]
 [assembly: System.Reflection.AssemblyProductAttribute("TodoApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TodoApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
